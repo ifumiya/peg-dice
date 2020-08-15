feab6b93a021d472d515167334c0dd39950ee90c
FODK <panoncotta@gmail.com>
2020-08-15 22:56:40 +0900

    一括インポート作成

---
+++ executors/Executor.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    AddExecutor: require('./AddExecutor'),
+    CommentExecutor: require('./CommentExecutor'),
+    CompareExecutor:  require('./CompareExecutor'),
+    DiceRoleExecutor:  require('./DiceRoleExecutor'),
+    NumberExecutor:  require('./NumberExecutor'),
+};
