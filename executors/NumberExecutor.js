f12b20e1bbc892a086243d5b7b7db862e4785891
FODK <panoncotta@gmail.com>
2020-08-15 22:28:17 +0900

    NumberExecutorを作成

---
+++ executors/NumberExecutor.js
@@ -0,0 +1,27 @@
+const AbstractExecutor = require("./AbstractExecutor");
+
+AbstractExecutor = require('./AbstractExecutor');
+
+class NumberExecutor extends AbstractExecutor
+{
+    value = 0
+
+    constructor(value)
+    {
+        this.value = value;
+    }
+
+    executeMine() 
+    { 
+        return this.value;
+    }
+
+    getExecutors() 
+    {
+        return { mine: this };
+    }
+}
+
+module.exports = {
+    NumberExecutor
+};
